65eec61ce58172245104fafcb2e0b41794b7190d
TonyShikhovtsev <ssvsr2@gmail.com>
2023-12-12 14:23:55 +0500

    24.1 Вьюсеты и дженерики (выполнение ДЗ)

---
+++ edu/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+from users.models import NULLABLE, User
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название курса')
+    image = models.ImageField(upload_to='courses/', verbose_name='изображение', **NULLABLE)
+    description = models.TextField(verbose_name='описание курса',  **NULLABLE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, **NULLABLE)
+
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название урока')
+    image = models.ImageField(upload_to='lessons/', verbose_name='изображение', **NULLABLE)
+    description = models.TextField(verbose_name='описание урока', **NULLABLE)
+    video_link = models.TextField(verbose_name='ссылка на видео',  **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+NULLABLE = {"null": True, "blank": True}
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone = models.CharField(max_length=35, verbose_name='Телефона', **NULLABLE)
+    city = models.CharField(max_length=70, verbose_name='Город', **NULLABLE)
+    avatar = models.ImageField(upload_to='products/', verbose_name='Аватар', **NULLABLE)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
