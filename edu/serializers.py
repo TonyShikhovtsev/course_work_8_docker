8fb10beea0d864258af465442e6bd5c280c681fe
TonyShikhovtsev <ssvsr2@gmail.com>
2023-12-17 22:49:02 +0500

    24.2 Выполнение ДЗ

--- edu/serializers.py
+++ edu/serializers.py
@@ -1,16 +1,15 @@
 from rest_framework import serializers
-
-from education.models import Course, Lesson
-
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-
+from edu.models import Course, Lesson
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
 
+class CourseSerializer(serializers.ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True)
 
+    class Meta:
+        model = Course
+        fields = ['id', 'name', 'image', 'description', 'user', 'lessons']
+    
